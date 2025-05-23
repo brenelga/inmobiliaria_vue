07f8109731fc4630a0c2108b12f01ad6299d7f1b
Brenel Galicia <jesus.galicia.23e@utzmg.edu.mx>
2025-05-23 10:54:45 -0600

    first commit

---
+++ src/utils/index.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export async function api() {
+    const options = {
+        method: 'GET',
+        url: ' http://localhost:8000/api/notificaciones/{departamentoId}',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    try{
+        const respuesta=await axios.request(options);
+        return respuesta.data;
+    } catch(error){
+        console.error('Error al conectarse a la API', error);
+        return [];
+    }
+}
---
+++ src/views/MultasView.vue
@@ -0,0 +1,24 @@
+<script>
+import { api } from '../utils/index.js'
+
+export default {
+  data() {
+    return {
+      multas: [],
+      departamentoId: '001'
+    }
+  },
+  mounted() {
+    this.cargarMultas()
+    this.interval = setInterval(this.cargarMultas, 5000)
+  },
+  beforeUnmount() {
+    clearInterval(this.interval)
+  },
+  methods: {
+    async cargarMultas() {
+      this.multas = await api(this.departamentoId)
+    }
+  }
+}
+</script>
